6dfc5c3262fef9013ed56669a14dea55986d2ca3
Petar Perić <petarperic93@gmail.com>
2018-10-05 19:55:06 +0200

    Change Image Script

---
+++ Assets/Augmented-Images-Standalone/Scripts/ButtonSpriteChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSpriteChange : MonoBehaviour {
+
+    public Sprite OffSprite;
+    public Sprite OnSprite;
+
+    public void ChageSprite()
+    {
+        Image image = GetComponent<Image>();
+        if (image.sprite == OnSprite)
+            image.sprite = OffSprite;
+        else
+        {
+            image.sprite = OnSprite;
+        }
+    }
+}
+
