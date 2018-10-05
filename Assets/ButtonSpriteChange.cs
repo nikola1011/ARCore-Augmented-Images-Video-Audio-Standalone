c2dded762b2430015aece57b96dfeac08ac76452
Petar Perić <petarperic93@gmail.com>
2018-10-05 19:31:18 +0200

    Pause icon on PlayPause button added
    Todo:
    Make sprite swap on button click
    Add more buttons and icons

---
+++ Assets/ButtonSpriteChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSpriteChange : MonoBehaviour {
+
+	public void ChageSprite()
+    {
+       //GetComponent<Image>
+       //TODO: Disable and enable sprite swap when button is clicked :) 
+    }
+}
